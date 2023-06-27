b55b08874912de4e0027ab447ce311817cbddb57
shubham <shubhamkumarhalwa@gmail.com>
2023-06-27 14:50:06 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   console.log('working fine on staging api')
-  res.send('testing staging backend micro service ok woking fine still roll back');
+  res.send('testing staging new backend micro service ok woking fine still roll back');
 });
 
 const port = process.env.PORT || 4000; 
