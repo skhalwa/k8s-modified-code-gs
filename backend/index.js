c11cc8c5d3ab37bab334157baa63d8670d51ec0d
shubham <shubhamkumarhalwa@gmail.com>
2023-06-27 14:26:31 +0530

    update files

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('working fine on staging backend')
-
-  res.send('+++++++++++++++++++++++++++++++++++! Backend API MICROSERVICE working testing !+++++++++++++++++++++++++++++++++++++++++++++++++++++');
+  console.log('working fine on staging api')
+  res.send('testing staging backend micro service ok woking fine still roll back');
 });
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4000; 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
