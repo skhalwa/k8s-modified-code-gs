df4e9966d0db5e723d7117508be0f859ff5909db
shubham <shubhamkumarhalwa@gmail.com>
2023-06-27 12:49:21 +0530

    index file

--- node/index.js
+++ node/index.js
@@ -2,12 +2,11 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  console.log('working fine on Staging')
-  res.send('Testign node on dev branch');
+  console.log('working fine on staging api')
+  res.send('testing staging api micro service ok woking fine still roll back');
 });
 
 const port = process.env.PORT || 3000; 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
